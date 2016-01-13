33d8e38d794a4e83c387011eb960f80b4a535155
lkgwu <leok@gwu.edu>
2016-01-13 15:30:00 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
 /* Hello world 
- * by Leo.
+ * by Leo Katz.
  */
 
 int main(int argc, char *argv[])
-{
+
+
+
+{	
+	int x, y, z, w, d, h, v; /*adding int for 3 new variables*/
+	x = 10;
+	y = 20;
+	w = 13;/*the width*/
+	d = 18;/*the depth*/
+	h = 14;/*the height*/
+	v = h*d*w;/*calculating the volumen multyplying the three */
+	z = x + y;
+	
 	printf("Hello world!\n");
+	printf("written by Leo N. Katz\n");
+	printf("x is %d, y is %d, and z is %d\n", x, y, z);
+	printf("Height is %d, Width is %d, Depth is %d, and Volume of the cube is %d\n", h, w, d, v); /*Got the idea from the top line*/
 	return 0;
+	
 }
